b8bfa66e367bd88301b1fa1d91829e44da7a042c
TomasKunka <tomas.kunka@gmail.com>
2024-09-23 10:38:12 +0000

    Pin providers + fix azcli auth

--- CHAP03/terraform/providers.tf
+++ CHAP03/terraform/providers.tf
@@ -4,19 +4,23 @@ terraform {
   required_providers {
     azapi = {
       source  = "azure/azapi"
-      version = "~>1.5"
+      version = "~>1.15.0"
     }
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = "~>3.0"
+      version = "~>3.116.0"
     }
     random = {
       source  = "hashicorp/random"
-      version = "~>3.0"
+      version = "~>3.6.0"
     }
     http = {
       source  = "hashicorp/http"
-      version = "~>3.0"
+      version = "~>3.4.0"
+    }
+    local = {
+      source  = "hashicorp/local"
+      version = "~>2.5.0"
     }
   }
   backend "azurerm" {}
@@ -24,4 +28,8 @@ terraform {
 
 provider "azurerm" {
   features {}
+}
+
+provider "azapi" {
+  use_msi = false
 }
